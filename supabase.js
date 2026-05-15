f58f553924bd2a5d6a82eddfa971fc6da2b3700f
Brandon <b.chamness@myyahoo.com>
2026-05-14 20:38:45 -0500

    fixed supabase.js config

--- supabase.js
+++ supabase.js
@@ -1,7 +1,2 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const SUPABASE_URL = 'https://hgevztkdrtajmqrmyguj.supabase.cO';
-const SUPABASE_KEY = 'sb_publishable_XK0JbG74rVe62-sLMrIdUA_w0ZYfCFY';
-
-const { createClient } = supabase;
-const db = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const SUPABASE_URL = 'https://hgevztkdrtajmqrmyguj.supabase.co';
+export const SUPABASE_KEY = 'sb_publishable_XK0JbG74rVe62-sLMrIdUA_w0ZYfCFY';
